bcdf4ea33f8c19ba5e49e8a5cf4e15015f40023f
Steve <stevegardiner26@aol.com>
2020-11-03 17:52:34 -0500

    Added a route to login and a route to create a user

---
+++ routes/userRoutes.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const User = mongoose.model('users');
+
+module.exports = (app) => {
+  // Get  
+  app.get(`/api/users/:id`, async (req, res) => {
+    const {id} = req.params;
+    let user = await User.findById(id);
+    return res.status(200).send(user);
+  });
+
+  // Create
+  app.post(`/api/users`, async (req, res) => {
+    let user = await User.create(req.body);
+    return res.status(201).send({
+      error: false,
+      user
+    })
+  })
+}
