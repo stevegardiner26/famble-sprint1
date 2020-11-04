55e9a450a55c2d04b75562511414cbe0fd9e093e
Steve <stevegardiner26@aol.com>
2020-11-03 19:56:22 -0500

    Added api route for creating/finding users on sign in

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -11,8 +11,10 @@ module.exports = (app) => {
 
   // Create
   app.post(`/api/users`, async (req, res) => {
-    // TODO: Potentially need to check if a user exists first before creating a new one (Use of google id here) (Still return the id)
-    let user = await User.create(req.body);
+    let user = await User.findOne({google_id: req.body.google_id});
+    if (!user) {
+        user = await User.create(req.body);
+    }
     return res.status(201).send({
       error: false,
       user
