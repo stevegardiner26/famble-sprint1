338042bbacacfe05a726c83f3df0d5907923cbdd
Steve <stevegardiner26@aol.com>
2020-11-03 19:57:42 -0500

    Created API Service Call for Signing In

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -4,5 +4,9 @@ export default {
   getCurrentUser: async () => {
     let res = await axios.get(`/api/users/${localStorage.user_id}`);
     return res.data || [];
-  }
+  },
+  signIn: async (payload) => {
+    let res = await axios.post(`/api/users`, payload);
+    return res.data || [];
+  } 
 }
