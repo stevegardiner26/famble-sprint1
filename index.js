78b1d4b9dfd9cd266df57a111e8f941f07f3974f
ranajay5699 <ranajay5699@gmail.com>
2020-11-05 14:31:35 -0500

    Run eslint fix on index.js

--- index.js
+++ index.js
@@ -4,29 +4,29 @@ const bodyParser = require('body-parser');
 
 // IMPORT MODELS
 require('./models/Game');
-require('./models/User')
+require('./models/User');
 
 const app = express();
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost:27017/node-react-starter`);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/node-react-starter');
 
 app.use(bodyParser.json());
 
-//IMPORT ROUTES
+// IMPORT ROUTES
 require('./routes/gameRoutes')(app);
 require('./routes/userRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-  
-    const path = require('path');
-    app.get('*', (req,res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`app running on port ${PORT}`)
+  console.log(`app running on port ${PORT}`);
 });
