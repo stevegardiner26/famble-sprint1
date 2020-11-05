3ddf6a3bf0bd60ef3d038e74216a9cb71f36c699
ranajay5699 <ranajay5699@gmail.com>
2020-11-04 23:09:53 -0500

    Remove index.css import

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import store from './store/store.js';
+import store from './store/store';
 
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import userService from './services/userService';
-import { login } from './store/slices/userSlice.js';
+import { login } from './store/slices/userSlice';
 
 // Checking if the user has been signed in already on this computer
 if (localStorage.user_id) {
   userService.getCurrentUser().then((response) => {
-    // TODO: If the response comes back bad we should remove the localStorage val and redirect the user to sign in again
+    // TODO: If the response comes back bad we should
+    // remove the localStorage val and redirect the user to sign in again
     store.dispatch(login(response));
   });
 }
