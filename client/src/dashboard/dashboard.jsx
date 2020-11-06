dad69c62dcfcb0b65a0531e254c33c7708bab314
VivekSreenivasan <vivek.sreenivasan3@gmail.com>
2020-11-06 16:40:24 -0500

    modify dsahsboard pages

--- client/src/dashboard/dashboard.jsx
+++ client/src/dashboard/dashboard.jsx
@@ -1,5 +1,10 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import Game from './game/game';
+
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
 
 function Dashboard(props) {
   return (
