e95af1e60d20e98409ede3c09816670c5be9024c
Pedro Ramos <pramos1996@Outlook.com>
2020-11-06 22:14:20 -0500

    Fixed Game.js and User.js to include time

--- models/Game.js
+++ models/Game.js
@@ -4,6 +4,25 @@ const {Schema} = mongoose;
 const gameSchema = new Schema({
     name: String,
     description: String,
+    sport_type: String,
+    start_time: {
+        type: Date,
+        default: ()=> new Date().toISOString()
+    },
+    winner: String,
+    team_one_id: String,
+    team_two_id: String,
+    timestamp: {
+        
+        created_at: {
+            type: Date,
+            default: ()=> new Date().toISOString()
+        },
+        updated_at: {
+            type: Date,
+            default: ()=> new Date().toISOString()
+        },
+    },
 })
 
 mongoose.model('games', gameSchema);
--- models/User.js
+++ models/User.js
@@ -3,9 +3,19 @@ const {Schema} = mongoose;
 
 const userSchema = new Schema({
     name: String,
-    email: String,
+    email: { type: String, unique: true },
     profile_image: String,
-    google_id: { type: String, unique: true }
+    google_id: String,
+    timestamp: {
+        created_at: {
+            type: Date,
+            default: ()=> new Date().toISOString()
+        },
+        updated_at: {
+            type: Date,
+            default: ()=> new Date().toISOString()
+        },
+    },
 })
 
 mongoose.model('users', userSchema);
