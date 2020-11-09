5be731f9346575bd5430558fbb785c96f2a51f2b
Pedro Ramos <pramos1996@Outlook.com>
2020-11-09 16:43:29 -0500

    Fixed routes to work with the gid_2_email

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,24 +1,24 @@
-const mongoose = require('mongoose');
 
+const mongoose = require('mongoose');
 const User = mongoose.model('users');
 
 module.exports = (app) => {
-  // Get
-  app.get('/api/users/:id', async (req, res) => {
-    const { id } = req.params;
-    const user = await User.findById(id);
+  // Get  
+  app.get(`/api/users/:id`, async (req, res) => {
+    const {id} = req.params;
+    let user = await User.findById(id);
     return res.status(200).send(user);
   });
 
   // Create
-  app.post('/api/users', async (req, res) => {
-    let user = await User.findOne({ google_id: req.body.google_id });
+  app.post(`/api/users`, async (req, res) => {
+    let user = await User.findOne({email: req.body.email});
     if (!user) {
-      user = await User.create(req.body);
+        user = await User.create(req.body);
     }
     return res.status(201).send({
       error: false,
-      user,
-    });
-  });
-};
+      user
+    })
+  })
+}
