f276f755acaae4c4196dee25f08f9c2375a1c2a2
ranajay5699 <ranajay5699@gmail.com>
2020-11-09 20:19:06 -0500

    Remove code that is no longer necessary

--- client/src/home/Home.jsx
+++ client/src/home/Home.jsx
@@ -22,27 +22,6 @@ function Home(props) {
     dispatch(login(res.user));
   };
 
-  // Code for getting the games -----------------
-  // const [games, setGames] = useState(null);
-  // const getGames = async () => {
-  //   const res = await gameService.getAll();
-  //   setGames(res);
-  // };
-
-  // useEffect(() => {
-  //   if (!games) {
-  //     getGames();
-  //   }
-  // });
-
-  // const renderGame = (game) => (
-  //   <li key={game._id} className="list__item game">
-  //     <h3 className="game__name">{game.name}</h3>
-  //     <p className="game__description">{game.description}</p>
-  //   </li>
-  // );
-  // -------------------
-
   return (
     <div>
       <GoogleLogin
@@ -50,14 +29,6 @@ function Home(props) {
           buttonText="Sign In"
           onSuccess={responseGoogle}
       />
-      {/* ---------Code for Game List----- */}
-      {/* <ul className="list">
-        {(games && games.length > 0) ? (
-          games.map((game) => renderGame(game))
-        ) : (
-          <p>No games found</p>
-        )}
-      </ul> */}
     </div>
   );
 }
