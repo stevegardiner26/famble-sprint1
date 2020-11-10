6a0ba49087670b8d88457478683a9002180b4563
VivekSreenivasan <vivek.sreenivasan3@gmail.com>
2020-11-10 16:38:36 -0500

    Put Table together

--- client/src/dashboard/Dashboard.jsx
+++ client/src/dashboard/Dashboard.jsx
@@ -5,11 +5,26 @@ import { selectUser, logout } from '../store/slices/userSlice';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 // import gameService from '../services/gameService';
-
+const useStyles = makeStyles((theme) => ({
+  table: {
+    minWidth:650,
+  },
+}));
+function createData(game, time, gameId){
+  return {game, time, gameId};
+}
 function Dashboard(props) {
+  const [games, setGames] = useState(null);
+  const classes = useStyles();
+ 
   const user = useSelector(selectUser);
-
   const dispatch = useDispatch();
 
    // Code to Handle Logout
@@ -52,7 +67,23 @@ function Dashboard(props) {
       <CssBaseline />
       <Container maxWidth="sm">
         <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '100vh' }}>
-        
+          <Table className = {classes.table} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Game</TableCell>
+              <TableCell align="right">Date</TableCell>
+              <TableCell align="right">Bet link</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {games.map((row)=>(
+              <TableRow key = {row.game}>
+                <TableCell align = "right"> {row.time}</TableCell>
+                <TableCell align = "right"><Button/></TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+          </Table>
         </Typography>
       </Container>
     </React.Fragment>
