72a603a1ff5e06e28a78c237306240e86aadb3c7
VivekSreenivasan <vivek.sreenivasan3@gmail.com>
2020-11-12 15:28:26 -0500

    Added access to getting team by id

--- client/src/services/gameService.js
+++ client/src/services/gameService.js
@@ -3,7 +3,13 @@ import axios from 'axios';
 export default {
   getAllGames: async () => {
     const res = await axios.get('/api/games');
+    
     return res.data || [];
   },
+  getTeam: async(id) =>{
+    const res = await axios.get(`/api/teams/${id}`);
+    return res.data || [];
+  }
+  
  
 };
