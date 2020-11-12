b04027cc40307796d8b3b342914072e4342851e9
Steve <stevegardiner26@aol.com>
2020-11-11 21:38:22 -0500

    Added/Adjusted Game Model

--- models/Game.js
+++ models/Game.js
@@ -3,13 +3,14 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const gameSchema = new Schema({
-    name: String,
-    description: String,
     sport_type: String,
     start_time: Date,
     winner: String,
-    team_one_id: String,
-    team_two_id: String
+    team_home_id: String,
+    team_away_id: String,
+    canceled: Boolean,
+    status: String,
+    score_id: Number
 }, {timestamps: true})
 
 mongoose.model('games', gameSchema);
