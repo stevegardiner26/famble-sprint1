9fbe5fd81e5879167c5727c977b6cd80fc9bb7d9
Steve <stevegardiner26@aol.com>
2020-11-11 22:15:31 -0500

    Added route to fetch all games for the season

--- routes/gameRoutes.js
+++ routes/gameRoutes.js
@@ -1,6 +1,8 @@
+var Client = require('node-rest-client').Client;
 const mongoose = require('mongoose');
-
 const Game = mongoose.model('games');
+
+var client = new Client();
 
 module.exports = (app) => {
   // Get
@@ -18,9 +20,52 @@ module.exports = (app) => {
     });
   });
 
+
+  app.get('/api/fetch_weekly_scores', async (req, res) => {
+    // Get Current Season
+    // Get Current Week
+    // Fetch Scores and Update Games with new data
+      //canceled: Boolean,
+      //    status: String,
+      //  away_score: Number,
+      //home_score: Number
+      // end_time
+      // calculate winner and select wininer if status is Final
+  });
+
   // Fetch Games From API
-  app.post('/api/fetch_games', async (req, res) => {
-    
+  // Ideally this is hit once a season to get the schedule
+  app.get('/api/fetch_games', async (req, res) => {
+    // Remove all games to prepare for the new game date
+
+    client.get("https://api.sportsdata.io/v3/nfl/scores/json/UpcomingSeason", {headers: {"Ocp-Apim-Subscription-Key": process.env['NFL_API_TOKEN']}}, function (year, response) {
+      client.get(`https://api.sportsdata.io/v3/nfl/scores/json/Schedules/${year}`, {headers: {"Ocp-Apim-Subscription-Key": process.env['NFL_API_TOKEN']}}, function (data, response) {
+        // parsed response body as js object
+        data.forEach(async (game) => {
+          let current_game = await Game.findOne({game_id: game.GlobalGameID});
+          if (!current_game) {
+            let payload = {
+              game_id: game.GlobalGameID,
+              sport_type: "NFL",
+              start_time: game.Date,
+              away_team_id: game.GlobalAwayTeamID,
+              home_team_id: game.GlobalHomeTeamID,
+              canceled: game.Canceled,
+              status: game.Status
+            };
+            await Game.create(payload);
+          } else {
+            let payload = {
+              start_time: game.Date,          
+              canceled: game.Canceled,
+              status: game.Status
+            };
+            await Game.findOneAndUpdate({game_id: game.GlobalGameID}, payload);
+          }
+        });
+      });
+      return res.status(201).send({message: "Imported All Games!"});
+    });
   });
 
   // Update
@@ -30,7 +75,7 @@ module.exports = (app) => {
     const game = await Game.findByIdAndUpdate(id, req.body);
 
     return res.status(202).send({
-      error: false,
+      error: false, 
       game,
     });
   });
