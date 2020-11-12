86b03a7d532007cd2f989a0e36bdc51d886686d3
Steve <stevegardiner26@aol.com>
2020-11-11 22:14:55 -0500

    Added team routes in backend to fetch all teams for the season

---
+++ routes/teamRoutes.js
@@ -0,0 +1,38 @@
+var Client = require('node-rest-client').Client;
+const mongoose = require('mongoose');
+const Team = mongoose.model('teams');
+
+var client = new Client();
+
+module.exports = (app) => {
+    app.get('/api/teams', async (req, res) => {
+        const teams = await Team.find();
+        return res.status(200).send(teams);
+    });
+
+    // Ideally this is hit once a season to get the teams
+    app.get('/api/fetch_teams', async (req, res) => {
+        
+        const teams = await Team.find();
+        teams.forEach(async (t) => {
+            await Team.findByIdAndDelete(t.id);
+        });
+
+        client.get(`https://api.sportsdata.io/v3/nfl/scores/json/Teams`, {headers: {"Ocp-Apim-Subscription-Key": process.env['NFL_API_TOKEN']}}, function (data, response) {
+            // parsed response body as js object
+            data.forEach(team => {
+                let payload = {
+                    name: team.FullName,
+                    team_id: team.TeamID,
+                    key: team.Key,
+                    conference: team.Conference,
+                    division: team.Division,
+                    stadium_id: team.StadiumID
+                };
+                Team.create(payload);
+            });
+        });
+        
+        return res.status(201).send({message: "Imported All Teams!"});
+    });
+}
