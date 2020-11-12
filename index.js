e2e540312e77dc0804bfcce149f634ff7f9a5073
Steve <stevegardiner26@aol.com>
2020-11-11 22:14:29 -0500

    Import dotenv to load env variables

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+require('dotenv').config({path: __dirname + '/.env'})
 
 // IMPORT MODELS
 require('./models/Game');
