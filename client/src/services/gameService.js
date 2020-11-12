e53a6b221bc79de5acb9b7386be396c2edfb2b34
VivekSreenivasan <vivek.sreenivasan3@gmail.com>
2020-11-12 11:18:24 -0500

    Added API handlers for games, teams, and scores

--- client/src/services/gameService.js
+++ client/src/services/gameService.js
@@ -1,8 +1,16 @@
 import axios from 'axios';
 // eslint-disable-next-line
 export default {
+  getAllGames: async () => {
+    const res = await axios.get('/api/fetch_games');
+    return res.data || [];
+  },
+  getAllTeams: async () => {
+    const res = await axios.get('/api/fetch_teams');
+    return res.data || [];
+  },
   getAll: async () => {
-    const res = await axios.get('/api/games');
+    const res = await axios.get('/api/fetch_weekly_scores');
     return res.data || [];
   },
 };
