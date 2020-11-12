c42ae298a99bba4c4324905311ebcd5c8fe2469f
Steve <stevegardiner26@aol.com>
2020-11-11 21:47:34 -0500

    Add more to Game Model

--- models/Game.js
+++ models/Game.js
@@ -3,11 +3,12 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const gameSchema = new Schema({
+    game_id: {type: Number, unique: true},
     sport_type: String,
     start_time: Date,
     winner: String,
-    team_home_id: String,
-    team_away_id: String,
+    team_home_id: Number,
+    team_away_id: Number,
     canceled: Boolean,
     status: String,
     score_id: Number
