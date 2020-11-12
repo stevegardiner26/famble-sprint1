4cbbe0586bada75b1cf2bf39fb4ff19a39d5f800
VivekSreenivasan <vivek.sreenivasan3@gmail.com>
2020-11-12 11:32:41 -0500

    Changes

--- client/src/services/gameService.js
+++ client/src/services/gameService.js
@@ -2,15 +2,8 @@ import axios from 'axios';
 // eslint-disable-next-line
 export default {
   getAllGames: async () => {
-    const res = await axios.get('/api/fetch_games');
+    const res = await axios.get('/api/games');
     return res.data || [];
   },
-  getAllTeams: async () => {
-    const res = await axios.get('/api/fetch_teams');
-    return res.data || [];
-  },
-  getAllScores: async () => {
-    const res = await axios.get('/api/fetch_weekly_scores');
-    return res.data || [];
-  },
+ 
 };
