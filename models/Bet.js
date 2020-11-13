ea23d80d4b73056591d44eca95c8bd88e4d7153b
VivekSreenivasan <vivek.sreenivasan3@gmail.com>
2020-11-13 17:19:20 -0500

    Linted game and bet

--- models/Bet.js
+++ models/Bet.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
-const {Schema} = mongoose;
+
+const { Schema } = mongoose;
 
 const betSchema = new Schema({
-    user_id: String,
-    game_id: String,
-    team_id: String,
-    amount: Number
-}, {timestamps: true})
+  user_id: String,
+  game_id: String,
+  team_id: String,
+  amount: Number,
+}, { timestamps: true });
 
 mongoose.model('bets', betSchema);
--- models/Game.js
+++ models/Game.js
@@ -3,18 +3,18 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const gameSchema = new Schema({
-    game_id: {type: Number, unique: true},
-    sport_type: String,
-    start_time: Date,
-    winner: String,
-    home_team_id: Number,
-    away_team_id: Number,
-    canceled: Boolean,
-    status: String,
-    away_score: Number,
-    home_score: Number,
-    in_progress: Boolean,
-    end_time: Date
-}, {timestamps: true})
+  game_id: { type: Number, unique: true },
+  sport_type: String,
+  start_time: Date,
+  winner: String,
+  home_team_id: Number,
+  away_team_id: Number,
+  canceled: Boolean,
+  status: String,
+  away_score: Number,
+  home_score: Number,
+  in_progress: Boolean,
+  end_time: Date,
+}, { timestamps: true });
 
 mongoose.model('games', gameSchema);
