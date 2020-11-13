61cc5373587b8306e1235627b927a221176ae6c7
VivekSreenivasan <vivek.sreenivasan3@gmail.com>
2020-11-13 14:45:29 -0500

    Added Pagination to make page more manageable

--- client/src/dashboard/Dashboard.jsx
+++ client/src/dashboard/Dashboard.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React, {useState, useEffect } from 'react';
-import { makeStyles } from '@material-ui/styles';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 
 import { useSelector, useDispatch } from 'react-redux';
@@ -17,6 +17,13 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 
+import IconButton from '@material-ui/core/IconButton';
+import FirstPageIcon from '@material-ui/icons/FirstPage';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+import LastPageIcon from '@material-ui/icons/LastPage';
+import TableFooter from '@material-ui/core/TableFooter';
+import TablePagination from '@material-ui/core/TablePagination';
 import BetModal from './BetModal';
 import Game from './game/Game';
 import { GoogleLogout } from 'react-google-login';
@@ -27,18 +34,89 @@ import styles from './Dashboard.module.css';
 // import gameService from '../services/gameService';
 const CLIENT_ID="405646879728-34aukb2l8lsknikc11pprr5i53pt3lvo.apps.googleusercontent.com"
 const useStyles = makeStyles((theme) => ({
-  table: {
-    minWidth:650,
+  root: {
+    flexShrink: 0,
+    marginLeft: '16px',
   },
 }));
+const useStyles2 = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+});
+
+
+
+
+function TablePaginationActions(props) {
+  const classes = useStyles();
+  const theme = useTheme();
+  const { count, page, rowsPerPage, onChangePage } = props;
+
+  const handleFirstPageButtonClick = (event) => {
+    onChangePage(event, 0);
+  };
+
+  const handleBackButtonClick = (event) => {
+    onChangePage(event, page - 1);
+  };
+
+  const handleNextButtonClick = (event) => {
+    onChangePage(event, page + 1);
+  };
+
+  const handleLastPageButtonClick = (event) => {
+    onChangePage(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+  };
+
+  return (
+    <div className={classes.root}>
+      <IconButton
+        onClick={handleFirstPageButtonClick}
+        disabled={page === 0}
+        aria-label="first page"
+      >
+        {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
+      </IconButton>
+      <IconButton onClick={handleBackButtonClick} disabled={page === 0} aria-label="previous page">
+        {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+      </IconButton>
+      <IconButton
+        onClick={handleNextButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="next page"
+      >
+        {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+      </IconButton>
+      <IconButton
+        onClick={handleLastPageButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="last page"
+      >
+        {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
+      </IconButton>
+    </div>
+  );
+}
 
 function Dashboard(props) {
+  
+  const [page, setPage] = React.useState(0);
   const [games, setGames] = useState([]);
-  const classes = useStyles();
- 
+  const classes = useStyles2();
+  const [rowsPerPage, setRowsPerPage] = React.useState(9);
+
+  const emptyRows = rowsPerPage - Math.min(rowsPerPage, games.length - page * rowsPerPage);
   const user = useSelector(selectUser);
   const dispatch = useDispatch();
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
 
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
    // Code to Handle Logout
   const handleLogout = () => {
     dispatch(logout());
@@ -101,12 +179,32 @@ function Dashboard(props) {
             </TableRow>
           </TableHead>
             <TableBody>
-              
-              {games.slice(165,games.length-1).map((row)=>(       
+            {(rowsPerPage > 0
+              ? games.slice(142,games.length-1).slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              : games
+            ).map((row) => (    
                 <Game info={row}/>
               ))}
               
             </TableBody>
+            <TableFooter>
+              <TableRow>
+              <TablePagination
+                  rowsPerPageOptions={[9,10, 25]}
+                  colSpan={3}
+                  count={games.length-141}
+                  rowsPerPage={rowsPerPage}
+                  page={page}
+                  SelectProps={{
+                    inputProps: { 'aria-label': 'rows per page' },
+                    native: true,
+                  }}
+                  onChangePage={handleChangePage}
+                  onChangeRowsPerPage={handleChangeRowsPerPage}
+                  ActionsComponent={TablePaginationActions}
+                />
+              </TableRow>
+            </TableFooter>
           </Table>
           </TableContainer>
         </Typography>
