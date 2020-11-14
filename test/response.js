564883514e741ff0a9e931de45d5dedff9034ebc
Pedro Ramos <pramos1996@Outlook.com>
2020-11-13 20:34:07 -0500

    creating unit-testing for the betServices

---
+++ test/response.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "error": false,
+    "bet": {
+        "_id": "5faf2ad7997c503640478eb1",
+        "user_id": "1",
+        "team_id": "234",
+        "amount": 20,
+        "game_id": "224",
+        "createdAt": "2020-11-14T00:54:47.123Z",
+        "updatedAt": "2020-11-14T00:54:47.123Z",
+        "__v": 0
+    }
+}
--- test/test.js
+++ test/test.js
@@ -1,8 +1,23 @@
-var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
+const chai = require('chai');
+const nock = require('nock');
+const expect = chai.expect;
+const response = require('./response');
+const getAllBets = require('../client/src/services/betService').getAll;
+
+describe('Bet Services', function() {
+    beforeEach(() => {
+      nock('localhost:5000')
+        .get('/api/bets')
+        .reply(response);
+    })
+});
+
+it('Get all bets', function() {
+  return getAllBets()
+    .then(response => {
+      // Test result of 
+      expect(response._id).to.equal('5faf2ad7997c503640478eb1');
+      expect(response.user_id).to.equal('5fac5f347d42705f0c14fbb0');
+      expect(response.team_id).to.equal('200');
     });
-  });
 });
