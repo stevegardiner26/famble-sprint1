6aafb6f610d4ef53686d8d294168734145a01262
Pedro Ramos <pramos1996@Outlook.com>
2020-11-19 10:56:37 -0500

    Added new functionality to betRoutes and betServices

--- routes/betRoutes.js
+++ routes/betRoutes.js
@@ -4,13 +4,15 @@ const Bet = mongoose.model('bets');
 const Game = mongoose.model('games');
 
 module.exports = (app) => {
-     // Get
+  // Get every single bet
   app.get('/api/bets', async (req, res) => {
     const bets = await Bet.find();
     return res.status(200).send(bets);
   });
 
-  app.get('/api/bets/:game_id', async (req, res) => {
+  // Bad practice, because it is biased towards User or the Game.
+
+  app.get('/api/bets/games/:game_id', async (req, res) => {
     const { game_id } = req.params;
 
     const bets = await Bet.find({game_id: game_id});
@@ -20,6 +22,28 @@ module.exports = (app) => {
       bets,
     });
   });
+
+  // Get every bet under User ID
+  app.get('/api/bets/users/:user_id', async (req, res) => {
+    const { user_id } = req.params;
+
+    const bets = await Bet.find({user_id: user_id});
+
+    return res.status(200).send({
+      error: false,
+      bets,
+    });
+  });
+
+  // Get a single Bet by ID
+
+  // app.get('/api/bets/:id', async (req, res) => {
+  //   const bet = await Bet.findById(req.params.id);
+  //   return res.status(200).send({
+  //     error: false,
+  //     bet,
+  //   });
+  // });
 
   // Create
   app.post('/api/bets', async (req, res) => {
@@ -33,18 +57,30 @@ module.exports = (app) => {
       bet,
     });
   });
-  
 
-
-//   // Update
-//   app.put('/api/bets/:id', async (req, res) => {
-//     const { id } = req.params;
-//     const bet = await Bet.findByIdAndUpdate(id, req.body);
-//     return res.status(202).send({
-//       error: false,
-//       bet,
-//     });
-//   });
+  // Update Amount
+  app.put('/api/bets/:id', async (req, res) => {
+    let bet = await Bet.findById(req.params.id);
+    const user = await User.findById(req.body.user_id);
+    if(req.body.updatedAmount > bet.amount){
+      await Bet.findByIdAndUpdate(req.params.id,{
+        amount: req.body.updatedAmount
+      });
+      await User.findByIdAndUpdate(req.body.user_id, {
+        shreddit_balance: user.shreddit_balance - (req.body.updatedAmount - bet.amount)
+      });
+      updatedBet = await Bet.findById(req.params.id);
+      return res.status(202).send({
+        error: false,
+        updatedBet,
+      });
+    }else{
+      return res.status(202).send({
+        error: true,
+        msg: "Could not update because the updated amount is the less than or equal to the current amount",
+      });
+    }
+  });
 
 //   // Delete
 //   app.delete('/api/bets/:id', async (req, res) => {
@@ -54,7 +90,7 @@ module.exports = (app) => {
 
 //     return res.status(202).send({
 //       error: false,
-//       bet,
-//     });
+//       bet,   5fac63bb93e6e411b0a84dd5 id
+//     }); 5fac5f347d42705f0c14fbb0 user id
 //   });
 };
